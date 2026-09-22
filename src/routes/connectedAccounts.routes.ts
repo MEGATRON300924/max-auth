9bb8f686e820854bce6a33b2e33bc360c906ac29
Zion Opaaje <zopaaje8@gmail.com>
2026-09-22 16:14:59 +0100

    Route Google Calendar OAuth flow

--- src/routes/connectedAccounts.routes.ts
+++ src/routes/connectedAccounts.routes.ts
@@ -5,11 +5,11 @@ import { oauthRateLimiter } from "../middleware/rateLimiter";
 
 const router = Router();
 
-router.get("/spotify/callback", connectedAccountsController.spotifyCallback);
+router.get("/spotify/callback", connectedAccountsController.spotifyCallback);\nrouter.get("/google/calendar/callback", connectedAccountsController.googleCalendarCallback);
 
 router.use(authenticate);
 
-router.get("/spotify/connect", oauthRateLimiter, connectedAccountsController.spotifyConnect);
+router.get("/spotify/connect", oauthRateLimiter, connectedAccountsController.spotifyConnect);\nrouter.get("/google/calendar/connect", oauthRateLimiter, connectedAccountsController.googleCalendarConnect);
 router.post("/spotify/refresh", oauthRateLimiter, connectedAccountsController.spotifyRefresh);
 
 /** @openapi /connected-accounts: get: tags: [Connected Accounts] summary: List linked third-party accounts */
