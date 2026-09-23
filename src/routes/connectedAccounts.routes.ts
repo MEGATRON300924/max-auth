ffadf32361f7ad575c2ad4e8d72a121e441c6de8
Zion Opaaje <zopaaje8@gmail.com>
2026-09-23 07:52:20 +0100

    Add Google Workspace API routes

--- src/routes/connectedAccounts.routes.ts
+++ src/routes/connectedAccounts.routes.ts
@@ -17,6 +17,34 @@ router.post("/google/calendar/events", connectedAccountsController.googleCalenda
 router.patch("/google/calendar/events/:eventId", connectedAccountsController.googleCalendarUpdateEvent);
 router.delete("/google/calendar/events/:eventId", connectedAccountsController.googleCalendarDeleteEvent);
 
+router.get("/google/drive/files", connectedAccountsController.googleDriveFiles);
+router.get("/google/drive/files/:fileId", connectedAccountsController.googleDriveFile);
+router.post("/google/drive/files", connectedAccountsController.googleDriveCreate);
+router.patch("/google/drive/files/:fileId", connectedAccountsController.googleDriveUpdate);
+router.delete("/google/drive/files/:fileId", connectedAccountsController.googleDriveDelete);
+
+router.get("/google/docs/:documentId", connectedAccountsController.googleDoc);
+router.post("/google/docs/:documentId:batchUpdate", connectedAccountsController.googleDocUpdate);
+
+router.get("/google/sheets/:spreadsheetId", connectedAccountsController.googleSheet);
+router.put("/google/sheets/:spreadsheetId/values", connectedAccountsController.googleSheetUpdate);
+
+router.get("/google/slides/:presentationId", connectedAccountsController.googleSlides);
+router.post("/google/slides/:presentationId:batchUpdate", connectedAccountsController.googleSlidesUpdate);
+
+router.get("/google/gmail/messages", connectedAccountsController.gmailMessages);
+router.get("/google/gmail/messages/:messageId", connectedAccountsController.gmailMessage);
+router.post("/google/gmail/messages/send", connectedAccountsController.gmailSend);
+router.post("/google/gmail/messages/:messageId/modify", connectedAccountsController.gmailModify);
+
+router.get("/google/tasks/lists", connectedAccountsController.googleTaskLists);
+router.get("/google/tasks", connectedAccountsController.googleTasks);
+router.post("/google/tasks", connectedAccountsController.googleTaskCreate);
+router.patch("/google/tasks/:taskId", connectedAccountsController.googleTaskUpdate);
+router.delete("/google/tasks/:taskId", connectedAccountsController.googleTaskDelete);
+
+router.get("/google/contacts", connectedAccountsController.googleContacts);
+
 router.get("/spotify/connect", oauthRateLimiter, connectedAccountsController.spotifyConnect);
 router.get("/google/calendar/connect", oauthRateLimiter, connectedAccountsController.googleCalendarConnect);
 router.post("/spotify/refresh", oauthRateLimiter, connectedAccountsController.spotifyRefresh);
