ea2d5945abc91aae38c55d38360644991ba83cdb
Zion Opaaje <zopaaje8@gmail.com>
2026-09-23 11:55:54 +0100

    Fix Google internal controller syntax

--- src/controllers/internalGoogle.controller.ts
+++ src/controllers/internalGoogle.controller.ts
@@ -71,7 +71,7 @@ export const internalGoogleController = {
   },
 
   async updateDoc(req: Request, res: Response, next: NextFunction) {
-    try { return ok(res, await connectedAccountsService.updateGoogleDoc(req.params.userId, req.params.documentId, Array.isArray(req.body?.requests) ? req.body.requests : []) }); }
+    try { return ok(res, await connectedAccountsService.updateGoogleDoc(req.params.userId, req.params.documentId, Array.isArray(req.body?.requests) ? req.body.requests : [])); } }
     catch (err) { next(err); }
   },
 
@@ -91,7 +91,7 @@ export const internalGoogleController = {
   },
 
   async updateSlides(req: Request, res: Response, next: NextFunction) {
-    try { return ok(res, await connectedAccountsService.updateGoogleSlides(req.params.userId, req.params.presentationId, Array.isArray(req.body?.requests) ? req.body.requests : []) }); }
+    try { return ok(res, await connectedAccountsService.updateGoogleSlides(req.params.userId, req.params.presentationId, Array.isArray(req.body?.requests) ? req.body.requests : [])); } }
     catch (err) { next(err); }
   },
 
